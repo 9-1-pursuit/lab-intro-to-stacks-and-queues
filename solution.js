b358d419fabc5157a99c40f78383a21279ba61aa
DestinyJoyner <destinyjoyner@pursuit.org>
2023-03-29 10:25:29 -0400

    node stack and queue class set up

--- solution.js
+++ solution.js
@@ -1,6 +1,27 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor(data, next = null){
+    this.data= data
+    this.next = next
+  }
+}
+
+class Stack {
+  constructor(top = null){
+    this.top = top
+  }
+}
+
+class Queue {
+  constructor(first = null, last = null){
+    this.first = first
+    this.last = last
+    this.size = 0
+  }
+}
+
 module.exports = {
   Node,
   Queue,
