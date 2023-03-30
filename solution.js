f64c93e91b31584270e71fcacc5d44bb77da06d2
Gillesedgy <edgygilles@pursuit.org>
2023-03-29 21:58:55 -0400

    enqueuE AND COUNT

--- solution.js
+++ solution.js
@@ -53,13 +53,42 @@ class Stack {
     }
     return min;
   }
-  peek(){
-    return this.top
+  peek() {
+    return this.top;
+  }
+  sort() {
+    const stack = new Stack();
+    while (!this.isEmpty()) {
+      const temp = this.pop();
+      while (!stack.isEmpty() && stack.peek() > temp) {
+        this.push(stack.pop());
+      }
+      stack.push(temp);
+    }
+    this.top = stack.top;
   }
 }
 
 class Queue {
-  constructor() {}
+  constructor() {
+    this.first = null
+    this.last = null;
+    this.size = 0;
+  }
+  count(){
+    return this.size;
+  }
+  enqueue(data) {
+    const newItem = new Node(data);
+    if (!this.first) {
+      this.first = newItem;
+      this.last = newItem;
+    } else {
+      this.last.next = newItem;
+      this.last = newItem;
+    }
+    return ++this.size;
+  }
 }
 
 module.exports = {
