564c2cb83d01391f219f999eaf348c710870730d
minagrullon <minagrullon@pursuit.org>
2023-03-29 22:30:02 -0400

    passes 17 tests minus sort

--- solution.js
+++ solution.js
@@ -1,6 +1,165 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(top = null) {
+    this.top = top;
+  }
+
+  push(data) {
+    const newItem = new Node(data);
+    newItem.next = this.top;
+    this.top = newItem;
+  }
+
+  pop() {
+    if (this.top == null) {
+      throw new Error("this stack is empty!");
+    }
+    let item = this.top;
+    if (item) {
+      let newItem = item.next;
+      this.top = newItem;
+
+      return item;
+    }
+  }
+
+  size() {
+    let count = 0;
+    let node = this.top;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+
+  isEmpty() {
+    return this.top === null;
+  }
+
+  findMin() {
+    let node = this.top;
+    let min = node.data;
+    while (node) {
+      if (node.data < min) {
+        min = node.data;
+      }
+      node = node.next;
+    }
+    return min;
+  }
+
+  peek() {
+    if (this.top == null) {
+      return null;
+    }
+    return this.top;
+  }
+
+  sort() {
+    const sortedStack = {
+      top: null,
+      isEmpty() {
+        return this.top === null;
+      },
+      push(data) {
+        this.top = { data, next: this.top };
+      },
+      pop() {
+        const top = this.top;
+        this.top = top.next;
+        return top.data;
+      },
+      peek() {
+        return this.top.data;
+      },
+    };
+
+    while (!this.isEmpty()) {
+      const current = this.pop();
+      while (!sortedStack.isEmpty() && sortedStack.peek() > current) {
+        this.push(sortedStack.pop());
+      }
+      sortedStack.push(current);
+    }
+
+    this.top = sortedStack.top;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+    this.max = null;
+  }
+
+  enqueue(data) {
+    let newItem = new Node(data);
+    if (!this.first) {
+      this.first = newItem;
+      this.last = newItem;
+    } else {
+      this.last.next = newItem;
+      this.last = newItem;
+    }
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (this.first === null) {
+      throw new Error("The queue is empty");
+    }
+    const item = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return item.data;
+  }
+
+  count() {
+    return this.size;
+  }
+
+  isEmpty() {
+    return this.first === null;
+  }
+  peek() {
+    if (this.first == null) {
+      throw new Error("The queue is empty");
+    }
+    return this.first;
+  }
+
+  findMax() {
+    let node = this.first;
+    let max = this.max;
+    while (node) {
+      if (node.data > max) {
+        max = node.data;
+      }
+      node = node.next;
+    }
+    return max;
+  }
+
+  getLast() {
+    return this.last;
+  }
+}
+
 module.exports = {
   Node,
   Queue,
