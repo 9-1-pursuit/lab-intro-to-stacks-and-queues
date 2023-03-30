22a766b1a1b438f30809ab2d91b88bdd9ad696c5
Gillesedgy <edgygilles@pursuit.org>
2023-03-29 20:36:29 -0400

    created node and stack class

--- solution.js
+++ solution.js
@@ -1,6 +1,20 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+class Stack {
+  constructor(top = null) {
+    this.top = top; // head
+    this.size = 0;
+  }
+}
+ 
+
 module.exports = {
   Node,
   Queue,
