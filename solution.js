1848e6becaa50d85ed7961021c590e8bb4c78f02
Gillesedgy <edgygilles@pursuit.org>
2023-03-30 09:57:39 -0400

    tried to sort wihtout Arr

--- solution.js
+++ solution.js
@@ -57,25 +57,32 @@ class Stack {
     return this.top;
   }
   sort() {
-    const stack = new Stack();
-    while (!this.isEmpty()) {
-      const temp = this.pop();
-      while (!stack.isEmpty() && stack.peek() > temp) {
-        this.push(stack.pop());
-      }
-      stack.push(temp);
+ //! Found a solution online wihtout using Array or Object => need to understand it a bit more :(
+      // const stack = new Stack();
+      // while (!this.isEmpty()) {
+      //   const temp = this.pop();
+      //   while (!stack.isEmpty() && stack.peek() > temp) {
+      //     this.push(stack.pop());
+      //   }
+      //   stack.push(temp);
+      // }
+      // this.top = stack.top;
+      // Empty the stack into an array
+      
+
     }
-    this.top = stack.top;
+ 
   }
-}
+
 
 class Queue {
   constructor() {
-    this.first = null
+    this.first = null;
     this.last = null;
     this.size = 0;
+    this.max = null;
   }
-  count(){
+  count() {
     return this.size;
   }
   enqueue(data) {
@@ -88,6 +95,39 @@ class Queue {
       this.last = newItem;
     }
     return ++this.size;
+  }
+  dequeue() {
+    if (this.first === null) {
+      throw new Error("The queue is empty");
+    }
+    const item = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return item.data;
+  }
+  findMax() {
+    let current = this.first;
+    let max = this.max;
+
+    while (current) {
+      if (current.data > max) {
+        max = current.data;
+      }
+      current = current.next;
+    }
+    return max;
+  }
+  isEmpty() {
+    return this.first === null;
+  }
+  peek() {
+    return this.first;
+  }
+  getLast() {
+    return this.last;
   }
 }
 
