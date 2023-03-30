170027267a89616855b520d5793a1fb8f15ef1ac
Gillesedgy <edgygilles@pursuit.org>
2023-03-29 21:06:21 -0400

    peek, findMin and isEmpty

--- solution.js
+++ solution.js
@@ -10,28 +10,56 @@ class Node {
 class Stack {
   constructor(top = null) {
     this.top = top; // head
-
+    // this.size = 0
   }
   push(data) {
     const newItem = new Node(data);
     newItem.next = this.top;
     this.top = newItem;
-   
+    //  this.size++
   }
+
   size() {
     let count = 0;
     let node = this.top;
-    while (node.next) {
+    while (node) {
       count++;
       node = node.next;
     }
     return count;
   }
-  
+  pop() {
+    if (this.top === null) {
+      return null;
+    }
+    let newItem = this.top;
+    this.top = newItem.next;
+    // this.size--
+    return newItem;
+  }
+
+  isEmpty() {
+    return this.top === null;
+  }
+  findMin() {
+    let current = this.top;
+    let min = current.data;
+
+    while (current) {
+      if (current.data < min) {
+        min = current.data;
+      }
+      current = current.next;
+    }
+    return min;
+  }
+  peek(){
+    return this.top
+  }
 }
 
 class Queue {
-  constructor(){}
+  constructor() {}
 }
 
 module.exports = {
