5c0632dfdef60c76195d44638d69d1262e8cc430
D-Stukes <dstukes888@gmail.com>
2023-04-05 20:04:22 -0400

    completed and passed 17 of 18 tests

--- solution.js
+++ solution.js
@@ -53,7 +53,6 @@ class Stack {
 
   findMin(){
     let node = this.top
-    // let nextNode = node.next
     let minValue = node.data
 
       while(node !== null){
@@ -65,34 +64,131 @@ class Stack {
       return minValue
   }
 
+  sort(data){
+    if (this.top === null) {
+      throw new Error("The stack is empty");
+    }
+    let item = this.top;  
+    let newItem = item.next;
+    let temp
+    let newList = []
+    while(item){
+      if (item > item.next) {
+        newList.push(item)
+        // temp = item
+        // item = newItem;
+        // newItem = temp
+      }
+    }
+    return ;
+  }
+
   // sort(){
   //   let node = this.top
-  //   // let nextNode = node.next
-  //   // let minValue = node.data
-  //   let maxValue = node.data
+  //   let previousNode
+    // let nextNode = node.next
+    // let minValue = node.data
+    // let minNode
+    // let lastNode 
 
-  //     while(node !== null){
-  //       if(node.data > maxValue){
-  //       maxValue = node.data
-  //       }
-  //       node = node.next
+
+  //   while(node !== null){
+  //     if(node.data < minValue){
+  //     this.top = minValue 
+
   //     }
-
-  //     return maxValue
+  //     previousNode = node
+  //     node = node.next
+  //   }
+  //     this.top = 
+  //     return minValue
   // }
-
-
-
 
 }
 
 class Queue {
-  constructor() {
+  constructor(first, last, size) {
     this.first = null;
     this.last = null;
     this.size = 0;
   }
+
+  enqueue(data) {
+    let newItem = new Node(data);
+    if (!this.first) {
+      this.first = newItem;
+      this.last = newItem;
+    } else {
+      this.last.next = newItem;
+      this.last = newItem;
+      
+    }
+    return ++this.size;
+  }
+    
+  count(){
+    return this.size
+  }
+
+  dequeue(data) {
+    if (this.first == null) {
+      throw new Error("The queue is empty");
+    }
+    const item = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return item.data;
+  }
+
+
+findMax(){
+  let maxValue = this.last.data
+  let node = this.first
+  while(node){
+   if(maxValue < node.data ){
+   maxValue = node.data
+    
+   }
+   node = node.next
+  }
+  return maxValue
 }
+
+
+
+getLast(){
+  return this.last
+}
+
+
+isEmpty(){
+  return this.first === null;
+
+}
+
+peek(){
+  return this.first
+}
+  // max(){
+  //   let node = this.first
+  //   let nextNode = this.next
+  //   let maxData
+
+  //   while(node !== null){
+  //    if(node.data > nextNode.data){
+  //      maxData = node.data
+  //    } else {
+  //      maxData = nextNode.data
+  //    }
+  //    node = node.next
+  //  }
+  //  return maxData
+  //  }
+  }
+
 
 module.exports = {
   Node,
